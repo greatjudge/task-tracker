c2156ad5fd950c0720dae408368f19b769f868ca
greatjudge <greatjudge0@gmail.com>
2023-02-25 17:07:54 +0300

    installed and used python-dotenv for load environment variables in settings.py changed secret_key

--- task_tracker/task_tracker/settings.py
+++ task_tracker/task_tracker/settings.py
@@ -10,17 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load environment variables
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-f5_f#!f^3rqg1%gvgh2(ly!2bt#wopa@!ww29f4wtt0jk&0dt+"
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
