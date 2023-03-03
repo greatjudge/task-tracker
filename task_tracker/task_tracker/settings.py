4da448faa581e0196fe71747f4a2b59d109c89ac
greatjudge <greatjudge0@gmail.com>
2023-03-03 21:18:39 +0300

    add cordeheaders

--- task_tracker/task_tracker/settings.py
+++ task_tracker/task_tracker/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "tasks",
     "users",
 ]
@@ -56,7 +57,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware"
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = "task_tracker.urls"
 
