6764ffa153142d99dfe7f7131028f6817e8c6b9e
greatjudge <greatjudge0@gmail.com>
2023-03-04 18:14:39 +0300

    Add linked_tasks validation and validation that start_time <= due_datetime

--- task_tracker/tasks/serializers.py
+++ task_tracker/tasks/serializers.py
@@ -13,12 +13,30 @@ class TaskSerializer(serializers.ModelSerializer):
         model = Task
         fields = '__all__'
 
+    def validate_linked_tasks(self, attrs):
+        print(attrs)
+        linked_task_userids = set([task.user.id for task in attrs])  # does it effective getting from db?
+        print(linked_task_userids)
+        if (len(linked_task_userids) != 1
+                or linked_task_userids.pop() != self.context['request'].user.id):
+            raise serializers.ValidationError('Related tasks must belong to the user')
+        return attrs
+
     def validate(self, data):
         user = self.context['request'].user
         if data['section'].user.id != user.id:
             raise serializers.ValidationError('Cannot create task with section created by another user')
         if data['status'].user.id != user.id:
             raise serializers.ValidationError('Cannot create task with status created by another user')
+
+        start_datetime = data['start_datetime'] if 'start_datetime' in data \
+            else self.instance.start_datetime
+        due_datetime = data.get('due_datetime')
+        if due_datetime is None:
+            if self.instance is not None and hasattr(self.instance, 'due_datetime'):
+                due_datetime = self.instance.due_datetime
+        if due_datetime is not None and start_datetime > due_datetime:
+            raise serializers.ValidationError("start_datetime could not be greater than due_datetime")
         return data
 
 
